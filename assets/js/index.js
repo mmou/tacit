3df130b84436eea7bae06e3177dcd7f2de57f468
Merry Mou <moumerry@gmail.com>
2015-04-27 13:50:49 -0400

    add tooltips

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,10 +22,10 @@ $(document).ready(function() {
   	    return sketch.slowDraw();
   	  }, 1500);
 
+  	// activate tooltips
+	$('[data-toggle="tooltip"]').tooltip()
 
-	// this code won't work now, but once easel/pad
-	// have been initialized, we should make "draw" the initial tool
-
+  	// make draw the default active tool 
 	$('.active').removeClass("active")
 	$("#draw-btn").addClass("active")
 	easel.currentTool = tacit.tools.draw
