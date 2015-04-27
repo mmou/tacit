b7b03f41b5ba5cd6e7c178fe628e07071a809f22
Eeway <eewayhsu@mit.edu>
2015-04-27 13:43:34 -0400

    magic number fix

--- assets/js/controllers/toolbar.js
+++ assets/js/controllers/toolbar.js
@@ -12,20 +12,20 @@ $('#ToolbarView').on('click', '.toolbar-btn', function(e) {
 		$("#PadView").css({'cursor': 'pointer'})
 	} else if (toolName === "draw-btn") {
 		easel.currentTool = tacit.tools.draw
-		$("#PadView").css({'cursor': 'url(assets/resources/cursor-images/pencil.png) 0 22, auto'})
+		$("#PadView").css({'cursor': 'url(assets/resources/cursor-images/pencil.png) 0 17, auto'})
 
 	} else if (toolName === "erase-btn") {
 		easel.currentTool = tacit.tools.erase
-		$("#PadView").css({'cursor': 'url(assets/resources/cursor-images/eraser.png) 0 22, auto'})
+		$("#PadView").css({'cursor': 'url(assets/resources/cursor-images/eraser.png) 0 18, auto'})
 
 	} else if (toolName === "measure-btn") {
 		easel.currentTool = tacit.tools.measure
-		$("#PadView").css({'cursor': 'url(assets/resources/cursor-images/ruler.png), auto'})
+		$("#PadView").css({'cursor': 'url(assets/resources/cursor-images/ruler.png) 0 20, auto'})
 
 
 	} else if (toolName === "test-btn") {
 		easel.currentTool = tacit.tools.test
-		$("#PadView").css({'cursor': 'url(assets/resources/cursor-images/weight.png), auto'})
+		$("#PadView").css({'cursor': 'url(assets/resources/cursor-images/weight.png) 10 10, auto'})
 
 	} else {
 		//Currently an issue with not pressing on image itself
