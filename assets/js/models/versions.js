40df4382007c63bcac662ce523bdc5b9d53c32e5
Edward Burnell <eburn@mit.edu>
2016-07-18 12:55:21 -0400

    whoops recompile

--- assets/js/models/versions.js
+++ assets/js/models/versions.js
@@ -64,6 +64,7 @@
       if (!(structure != null)) {
         structure = new tacit.Structure(this.project.easel.pad.sketch.structure);
       }
+      window.log += ("# at " + (new Date().toLocaleString()) + ", a new structure of weight " + structure.lp.obj + " with " + project.easel.pad.sketch.structure.nodeList.length + " nodes and " + project.easel.pad.sketch.structure.beamList.length + " beams was created by the save tool\n") + structure.strucstr + "\n";
       this.project.easel.pad.sketch.fea();
       versionObj = d3.select(this.htmlLoc).append("div").attr("id", "ver" + this.history.length).classed("ver", true);
       easel = new dummyEasel(this, this.history.length, this.project);
@@ -91,9 +92,8 @@
         window.triggers.save();
       }
       if (this.project.actionQueue.length > 1 || (structure != null)) {
-        this.newVersion(structure);
+        return this.newVersion(structure);
       }
-      return window.log += ("# at " + (new Date().toLocaleString()) + ", a new structure of weight " + structure.lp.obj + " with " + project.easel.pad.sketch.structure.nodeList.length + " nodes and " + project.easel.pad.sketch.structure.beamList.length + " beams was created by the save tool\n") + structure.strucstr + "\n";
     };
 
     return Versions;
